06e58046c08b24ed79e58f24cb255b402ca10ac7
Viktoria Talmaza <vp.talmaza@gmail.com>
2020-11-12 10:36:05 +0300

    test handler init

---
+++ application/custom_experience/usecase/usecase_test.go
@@ -0,0 +1,52 @@
+package usecase
+
+import (
+	"github.com/apsdehal/go-logger"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/models"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/testing/mocks/application/custom_experience"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func beforeTest() (*mocks.CustomExperienceRepository, UseCase) {
+	infoLogger, _ := logger.New("test", 1, os.Stdout)
+	errorLogger, _ := logger.New("test", 2, os.Stderr)
+	mockRepo := new(mocks.CustomExperienceRepository)
+	usecase := UseCase{
+		infoLogger:  infoLogger,
+		errorLogger: errorLogger,
+		customExperienceRepository:        mockRepo,
+	}
+	return mockRepo, usecase
+}
+
+var ID, _ = uuid.Parse("77b2e989-6be6-4db5-a657-f25487638af9")
+var testExperience = models.ExperienceCustomComp{
+	ID:              ID,
+	CandID:          ID,
+	ResumeID:        ID,
+	NameJob:         "",
+}
+
+func TestEducationCreate(t *testing.T) {
+	mockRepo, usecase := beforeTest()
+
+	mockRepo.On("Create", testExperience).Return(&testExperience, nil)
+	answer, err := usecase.Create(testExperience)
+	assert.Nil(t, err)
+	assert.Equal(t, *answer, testExperience)
+}
+
+func TestEducationDropAll(t *testing.T) {
+	mockRepo, usecase := beforeTest()
+
+	mockRepo.On("DropAllFromResume", ID).Return(nil)
+	err := usecase.DropAllFromResume(ID)
+	assert.Nil(t, err)
+
+	mockRepo.On("DropAllFromResume", uuid.Nil).Return(assert.AnError)
+	err2 := usecase.DropAllFromResume(uuid.Nil)
+	assert.Error(t, err2)
+}
---
+++ application/resume/delivery/http/handler_test.go
@@ -0,0 +1,103 @@
+package http
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/common"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/models"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/testing/general"
+	mocksCommon "github.com/go-park-mail-ru/2020_2_MVVM.git/testing/mocks/application/common"
+	mocksExp "github.com/go-park-mail-ru/2020_2_MVVM.git/testing/mocks/application/custom_experience"
+	mocksEduc "github.com/go-park-mail-ru/2020_2_MVVM.git/testing/mocks/application/education"
+	mocksResume "github.com/go-park-mail-ru/2020_2_MVVM.git/testing/mocks/application/resume"
+	"github.com/google/uuid"
+	"net/http"
+	"os"
+	"testing"
+)
+
+const (
+	resumeUrlGroup = "/api/v1/resume/"
+)
+
+var testData struct {
+	resumeHandler *ResumeHandler
+	router        *gin.Engine
+	mockUseCase   *mocksResume.UseCase
+	mockUSEduc		*mocksEduc.UseCase
+	mockUSExp		*mocksExp.UseCase
+	mockAuth      *mocksCommon.AuthTest
+	httpStatus    []int
+	vacList       []models.Resume
+}
+
+func TestMain(m *testing.M) {
+	setUp()
+	retCode := m.Run()
+	os.Exit(retCode)
+}
+
+func setUp() {
+	gin.SetMode(gin.TestMode)
+	testData.httpStatus = []int{
+		http.StatusOK,
+		http.StatusBadRequest,
+		http.StatusInternalServerError,
+	}
+	testData.vacList = []models.Resume{
+		{Title: "title1", Description: "description1"},
+		{Title: "title2", Description: "description2"},
+		{Title: "title3", Description: "description3"}}
+	testData.mockUseCase = new(mocksResume.UseCase)
+	testData.mockUSEduc = new(mocksEduc.UseCase)
+	testData.mockUSExp = new(mocksExp.UseCase)
+	testData.router = gin.Default()
+	api := testData.router.Group("api/v1")
+	testData.resumeHandler = NewRest(api.Group("/resume"), testData.mockUseCase, testData.mockUSEduc, testData.mockUSExp, nil)
+}
+
+func getRespStruct(entity interface{}) interface{} {
+	switch entity.(type) {
+	case models.Resume:
+		resume := entity.(models.Resume)
+		return &resume
+	case []models.Resume:
+		resumeList := entity.([]models.Resume)
+		return resumeList
+	case string:
+		err := entity.(string)
+		return common.RespError{Err: err}
+	case error:
+		err := entity.(error)
+		return common.RespError{Err: err.Error()}
+	}
+	return nil
+}
+
+func TestGetResumeByIdHandler(t *testing.T) {
+	h, r, mockUseCase := testData.resumeHandler, testData.router, testData.mockUseCase
+	r.GET("/by/id/:resume_id", h.GetResumeByID)
+	ID := uuid.New()
+	resume := models.Resume{ResumeID: ID}
+	mockUseCase.On("GetById", ID).Return(&resume, nil)
+	//mockUseCase.On("GetVacancy", uuid.Nil).Return(nil, assert.AnError)
+
+	testUrls := []string{
+		fmt.Sprintf("%sby/id/%s", resumeUrlGroup, ID),
+		//fmt.Sprintf("%sby/id/invalidUuid", resumeUrlGroup),
+		//fmt.Sprintf("%sby/id/%s", resumeUrlGroup, uuid.Nil),
+	}
+	testExpectedBody := []interface{}{resume, common.EmptyFieldErr, common.DataBaseErr}
+
+	for i := range testUrls {
+		t.Run("test responses on different urls for getVacancy handler", func(t *testing.T) {
+			w, err := general.PerformRequest(r, http.MethodGet, testUrls[i], nil)
+			if err != nil {
+				t.Fatalf("Couldn't create request: %v\n", err)
+			}
+			if err := general.ResponseComparator(*w, testData.httpStatus[i], getRespStruct(testExpectedBody[i])); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
