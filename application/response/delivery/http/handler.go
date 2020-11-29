9d527b4467d29afa400519df1a1d45cea5f972b2
H-b-IO-T-O-H <importantv@yandex.ru>
2020-11-30 01:27:30 +0300

    add

--- application/response/delivery/http/handler.go
+++ application/response/delivery/http/handler.go
@@ -261,7 +261,7 @@ func (r *ResponseHandler) handlerGetAllNotifications(ctx *gin.Context) {
 		notifications.UnreadRespCnt = uint(len(notifications.UnreadResp))
 	}
 	unId, _ = common.GetCurrentUserId(session, common.UserID)
-	if req.OnlyVacCnt {
+	if req.OnlyVacCnt && req.VacInLastNDays != nil {
 		if daysFromNow == 0 {
 			daysFromNow = common.Week
 		}
