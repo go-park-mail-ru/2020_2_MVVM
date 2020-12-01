686194a022adc1ce261194abc90509b0ece8ebc5
H-b-IO-T-O-H <importantv@yandex.ru>
2020-12-01 05:10:13 +0300

    save

---
+++ application/microservices/vacancy/server/vacancy.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"context"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/microservices/vacancy/api"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/models"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/vacancy"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type vacServer struct {
+	vacUseCase vacancy.IUseCaseVacancy
+	api.UnimplementedVacancyServer
+}
+
+func NewVacServer(vacUseCase vacancy.IUseCaseVacancy) api.VacancyServer {
+	return &vacServer{vacUseCase: vacUseCase}
+}
+
+func (v *vacServer) CreateVacancy(ctx context.Context, req *api.Vac) (*api.Vac, error) {
+	vacReq := models.Vacancy{Title: req.Title, SalaryMin: int(req.SalaryMin), SalaryMax: int(req.SalaryMax), AreaSearch: req.AreaSearch,
+		Description: req.Description, Requirements: req.Requirements, Duties: req.Duties, Skills: req.Skills, Employment: req.Employment,
+		ExperienceMonth: int(req.ExperienceMonth), Location: req.Location, CareerLevel: req.CareerLevel, EducationLevel: req.EducationLevel,
+		EmpPhone: req.EmpPhone, EmpEmail: req.EmpEmail, Gender: req.Gender}
+	if req.Sphere == "" {
+		vacReq.Sphere = -1
+	}
+	newVac, err := v.vacUseCase.CreateVacancy(vacReq)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &api.Vac{Id: newVac.ID.String(), Title: newVac.Title, SalaryMin: uint32(newVac.SalaryMin), SalaryMax: uint32(newVac.SalaryMax), AreaSearch: newVac.AreaSearch,
+		Description: newVac.Description, Requirements: newVac.Requirements, Duties: newVac.Duties, Skills: newVac.Skills, Employment: newVac.Employment,
+		ExperienceMonth: uint32(newVac.ExperienceMonth), Location: newVac.Location, CareerLevel: newVac.CareerLevel, EducationLevel: newVac.EducationLevel,
+		EmpPhone: newVac.EmpPhone, EmpEmail: newVac.EmpEmail, Gender: newVac.Gender}, nil
+}
+
+func (v *vacServer) GetVacancy(ctx context.Context, vacId *api.VacId) (*api.Empty, error) {
+	/*id, _ := uuid.Parse(vacId.Id)
+	newVac, err := v.vacUseCase.GetVacancy(id)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &api.Vac{Id: newVac.ID.String(), Title: newVac.Title, SalaryMin: uint32(newVac.SalaryMin), SalaryMax: uint32(newVac.SalaryMax), AreaSearch: newVac.AreaSearch,
+		Description: newVac.Description, Requirements: newVac.Requirements, Duties: newVac.Duties, Skills: newVac.Skills, Employment: newVac.Employment,
+		ExperienceMonth: uint32(newVac.ExperienceMonth), Location: newVac.Location, CareerLevel: newVac.CareerLevel, EducationLevel: newVac.EducationLevel,
+		EmpPhone: newVac.EmpPhone, EmpEmail: newVac.EmpEmail, Gender: newVac.Gender}, nil*/
+	return nil, nil
+}
---
+++ application/microservices/vacancy/vacancyMicro/vacancy.go
@@ -0,0 +1,79 @@
+package vacancyMicro
+
+import (
+	"context"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/common"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/microservices/vacancy/api"
+	"github.com/go-park-mail-ru/2020_2_MVVM.git/application/models"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"strconv"
+)
+
+type gRPCVacClient struct {
+	client api.VacancyClient
+	gConn  *grpc.ClientConn
+	logger common.Logger
+	ctx    context.Context
+}
+
+func (g *gRPCVacClient) CreateVacancy(vacancy models.Vacancy) (*models.Vacancy, error) {
+	newVac, err := g.client.CreateVacancy(g.ctx, &api.Vac{Id: vacancy.ID.String(), Title: vacancy.Title, SalaryMin: uint32(vacancy.SalaryMin), SalaryMax: uint32(vacancy.SalaryMax), AreaSearch: vacancy.AreaSearch,
+		Description: vacancy.Description, Requirements: vacancy.Requirements, Duties: vacancy.Duties, Skills: vacancy.Skills, Employment: vacancy.Employment,
+		ExperienceMonth: uint32(vacancy.ExperienceMonth), Location: vacancy.Location, CareerLevel: vacancy.CareerLevel, EducationLevel: vacancy.EducationLevel,
+		EmpPhone: vacancy.EmpPhone, EmpEmail: vacancy.EmpEmail, Gender: vacancy.Gender})
+	if newVac == nil {
+		return nil, err
+	}
+	vacResp := models.Vacancy{Title: newVac.Title, SalaryMin: int(newVac.SalaryMin), SalaryMax: int(newVac.SalaryMax), AreaSearch: newVac.AreaSearch,
+		Description: newVac.Description, Requirements: newVac.Requirements, Duties: newVac.Duties, Skills: newVac.Skills, Employment: newVac.Employment,
+		ExperienceMonth: int(newVac.ExperienceMonth), Location: newVac.Location, CareerLevel: newVac.CareerLevel, EducationLevel: newVac.EducationLevel,
+		EmpPhone: newVac.EmpPhone, EmpEmail: newVac.EmpEmail, Gender: newVac.Gender}
+	return &vacResp, err
+}
+
+func (g *gRPCVacClient) UpdateVacancy(vacancy models.Vacancy) (*models.Vacancy, error) {
+	//panic("implement me")
+	return nil, nil
+}
+
+func (g *gRPCVacClient) GetVacancy(vacId uuid.UUID) (*models.Vacancy, error) {
+	id := api.VacId{Id: vacId.String()}
+	newVac, err := g.client.GetVacancy(g.ctx, &id)
+	if newVac == nil {
+		return nil, err
+	}
+	/*vacResp := models.Vacancy{Title: newVac.Title, SalaryMin: int(newVac.SalaryMin), SalaryMax: int(newVac.SalaryMax), AreaSearch: newVac.AreaSearch,
+		Description: newVac.Description, Requirements: newVac.Requirements, Duties: newVac.Duties, Skills: newVac.Skills, Employment: newVac.Employment,
+		ExperienceMonth: int(newVac.ExperienceMonth), Location: newVac.Location, CareerLevel: newVac.CareerLevel, EducationLevel: newVac.EducationLevel,
+		EmpPhone: newVac.EmpPhone, EmpEmail: newVac.EmpEmail, Gender: newVac.Gender}*/
+	return nil, err
+}
+
+func (g *gRPCVacClient) GetVacancyList(u uint, u2 uint, uuid uuid.UUID, i int) ([]models.Vacancy, error) {
+	return nil, nil
+}
+
+func (g *gRPCVacClient) SearchVacancies(params models.VacancySearchParams) ([]models.Vacancy, error) {
+	return nil, nil
+}
+
+func (g *gRPCVacClient) AddRecommendation(uuid uuid.UUID, i int) error {
+	return nil
+}
+
+func (g gRPCVacClient) GetRecommendation(uuid uuid.UUID, i int, i2 int) ([]models.Vacancy, error) {
+	return nil, nil
+}
+
+func NewVacClient(host string, port int, logger common.Logger) (VacClient, error) {
+	gConn, err := grpc.Dial(
+		host+":"+strconv.Itoa(port),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &gRPCVacClient{client: api.NewVacancyClient(gConn), gConn: gConn, logger: logger, ctx: context.Background()}, nil
+}
